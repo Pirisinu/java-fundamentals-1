24a82c1899df50090d469527d119e5dbc924018b
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-27 14:18:26 +0100

    add class CalcolaBiglietto with generic pseudocode

---
+++ src/org/experis/treno/CalcolaBiglietto.java
@@ -0,0 +1,12 @@
+package org.experis.treno;
+
+public class CalcolaBiglietto {
+    // Importare lo scanner per ricevere gli input dall'utente
+
+    // Chiedere all'utente numero chilometri e la sua età
+
+    // Il prezzo del tiket è 0.21€
+
+    // Va applicato uno sconto del 20% ai minorenni e un 40% per gli over65
+
+}
