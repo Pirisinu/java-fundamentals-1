f746d54267d1be9ce976727b776a457ad30ca6e3
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-27 15:43:42 +0100

    add invited array + scanner

--- src/org/experis/bonus/festaFerragnez/FestaFerragnez.java
+++ src/org/experis/bonus/festaFerragnez/FestaFerragnez.java
@@ -1,7 +1,25 @@
 package org.experis.bonus.festaFerragnez;
 
+import java.util.Scanner;
+
 public class FestaFerragnez {
     public static void main(String[] args) {
+        // Inizzializzare l'array con i nomi degli inviati
+        String[] invited = {
+                "Dua Lipa",
+                "Paris Hilton",
+                "Manuel Agnelli",
+                "J-Ax",
+                "Francesco Totti",
+                "Ilary Blasi",
+                "Bebe Vio",
+                "Luis",
+                "Pardis Zarei",
+                "Martina Maccherone",
+                "Rachel Zeilic"};
+
+        // Importo lo scanner per ricevere in seguito il nome dell'utente
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
