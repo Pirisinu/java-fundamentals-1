c75de1005c360c0b585b0e5da4451f8bba5864f2
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-27 14:27:16 +0100

    import Scanner

--- src/org/experis/treno/CalcolaBiglietto.java
+++ src/org/experis/treno/CalcolaBiglietto.java
@@ -1,8 +1,11 @@
 package org.experis.treno;
 
+
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
     // Importare lo scanner per ricevere gli input dall'utente
-
+    Scanner scanner = new Scanner(System.in);
     // Chiedere all'utente numero chilometri e la sua età
 
     // Il prezzo del tiket è 0.21€
