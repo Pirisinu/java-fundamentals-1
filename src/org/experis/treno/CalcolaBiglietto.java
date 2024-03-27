28e0c3e31dc44d731931db1450f168f82088d3ec
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-27 14:38:31 +0100

    get input age and km from scanner

--- src/org/experis/treno/CalcolaBiglietto.java
+++ src/org/experis/treno/CalcolaBiglietto.java
@@ -8,7 +8,13 @@ public class CalcolaBiglietto {
         // Importare lo scanner per ricevere gli input dall'utente
         Scanner scanner = new Scanner(System.in);
         // Chiedere all'utente numero chilometri e la sua età
-
+        int kmSelected, userAge;
+        System.out.println("Quanti chilometri devi percorrere?");
+        kmSelected = scanner.nextInt();
+        System.out.println("Quanti anni hai?");
+        userAge = scanner.nextInt();
+        System.out.println("km selezionati: " + kmSelected);
+        System.out.println("eta : " + userAge);
         // Il prezzo del tiket è 0.21€
 
         // Va applicato uno sconto del 20% ai minorenni e un 40% per gli over65
