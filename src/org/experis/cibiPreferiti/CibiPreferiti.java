63d3cf01291028be5d06a37edf0a23dbb3b73b73
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-27 15:10:09 +0100

    add array piattiPreferiti

--- src/org/experis/cibiPreferiti/CibiPreferiti.java
+++ src/org/experis/cibiPreferiti/CibiPreferiti.java
@@ -3,5 +3,18 @@ package org.experis.cibiPreferiti;
 public class CibiPreferiti {
     public static void main(String[] args) {
 
+        // Inizzializzare l'array
+        String[] piattiPreferiti = {
+                "Spaghetti alla carbonara",
+                "Risotto ai funghi porcini",
+                "Lasagne alla bolognese",
+                "Penne all'arrabbiata",
+                "Gnocchi alla sorrentina",
+                "Minestrone",
+                "Ravioli ricotta e spinaci"
+        };
+
+
+
     }
 }
