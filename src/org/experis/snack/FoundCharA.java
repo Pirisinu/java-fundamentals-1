55e6e0f0a57c1523538dc15317cbf8f0ec925875
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 12:27:21 +0100

    init scnak FoundCharA + import Scanner

---
+++ src/org/experis/snack/FoundCharA.java
@@ -0,0 +1,12 @@
+package org.experis.snack;
+
+import java.util.Scanner;
+
+public class FoundCharA {
+    // CONSEGNA
+    // Data una stringa inserita dall’utente contare quante volte appare il carattere ‘a’ (minuscolo o maiuscolo)
+    public static void main(String[] args) {
+        // Importo lo scanner
+        Scanner scan = new Scanner(System.in);
+    }
+}
