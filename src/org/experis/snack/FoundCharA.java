108908b31f5bb194733fadebf070b56fa2e8a9f3
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 12:36:36 +0100

    add counter

--- src/org/experis/snack/FoundCharA.java
+++ src/org/experis/snack/FoundCharA.java
@@ -8,5 +8,15 @@ public class FoundCharA {
     public static void main(String[] args) {
         // Importo lo scanner
         Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci testo a piacimento");
+        String inputString = scan.nextLine();
+        char[] listChar = inputString.toCharArray();
+        int counter = 0;
+        for (char c : listChar){
+            if (c == 'a'){
+                counter++;
+            }
+        }
+        System.out.println("Numero di volte che ho trovalo la lettera a:" + counter  );
     }
 }
